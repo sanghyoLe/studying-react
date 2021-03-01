99ed248f85571f9f338757053d7f42f23282d540
SangHyo <gilsport1@gmail.com>
2021-03-01 18:46:57 +0900

    create RockScissorsPaper

---
+++ self-studying/RockScissorsPaper/client.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import RSP from './RSP'
+
+
+
+ReactDOM.render(<RSP />,document.querySelector('#root'));
+
---
+++ self-studying/RockScissorsPaper/webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const webpack = require('webpack');
+const RefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+module.exports = {
+  name: 'rockScissorsPaper-setting',
+  mode: 'development', // 실서비스 : production
+  devtool : 'eval', 
+  resolve: {
+        extensions: ['.js','.jsx']
+  },
+  entry: {
+    app: ['./client'],
+  }, // input
+
+  module: {
+    rules: [
+      {
+        test: /.\jsx?/,
+        loader: "babel-loader",
+        options: {
+          presets: [
+            [ "@babel/preset-env",{
+                targets: {
+                  browsers: [' > 1% in KR'],
+                },
+                debug: true,
+              }],
+            "@babel/preset-react",
+            
+            
+          ],
+         
+          plugins: ['@babel/plugin-proposal-class-properties',
+                    'react-refresh/babel'],
+        },
+      }],
+  },
+  plugins : [
+    new webpack.LoaderOptionsPlugin({debug: true}),
+    new RefreshWebpackPlugin(),
+],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'app.js',
+    publicPath:'/dist/',
+  }, // output
+  devServer: {
+    publicPath:'/dist/',
+    hot:true,
+  },
+
+};
