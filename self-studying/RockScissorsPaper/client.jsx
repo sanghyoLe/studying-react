f399809d7d13b1f2973ad40cd8c3297cc4097ff6
SangHyo <gilsport1@gmail.com>
2021-03-05 18:55:41 +0900

    create RSP_hooks

--- self-studying/RockScissorsPaper/client.jsx
+++ self-studying/RockScissorsPaper/client.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import RSP from './RSP'
+import RSP_hooks from './RSP_hooks'
 
 
 
-ReactDOM.render(<RSP />,document.querySelector('#root'));
+// ReactDOM.render(<RSP />,document.querySelector('#root'));
+ReactDOM.render(<RSP_hooks/>,document.querySelector('#root'));
 
